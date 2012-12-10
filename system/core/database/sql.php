451d9ac140333417b84a47a136a92ccf189e60ed
John Papikas <papikas.ioan@gmail.con>
2012-12-10 20:56:01 +0200

    Commit and Sync

--- system/core/database/sql.php
+++ system/core/database/sql.php
@@ -5,7 +5,20 @@
  *
  */
 
-class sql
-{
+class SqlQueryCreator {
+    
+    public function select(array $tblColsMap = array()) {
+        
+      $stmt = empty($tblColsMap) ? 'SELECT *' : 'SELECT '; 
+      
+      $tblColPairs = array();
+      foreach ($tblColsMap as $tableName => $cols) {
+          foreach ($cols as $columnName) {
+              $tblColPairs[] = '`' . $tableName . '`' . '.`' . $columnName . '`';
+          }
+      }
+      $stmt .= implode(',' , $tblColPairs);
+      return $stmt;
+    }
 }
 ?>
