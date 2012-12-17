890b7bfdac84e9300cdf3eb2cb235e8ec18af6d0
mariossotir <mai1211@uom.gr>
2012-12-17 20:28:43 +0200

    Rename domain/user.php to usermodel.php.
    Signed-off-by: mariossotir <mai1211@uom.gr>

---
+++ system/core/domain/usermodel.php
@@ -0,0 +1,33 @@
+<?php
+
+// System Check
+if (!defined("_WBCHAT_PLATFORM_")) throw new Exception("Web Platform is not defined!");
+
+// Imports
+importer::importCore("database::dbConnection");
+
+/**
+ * Description of User
+ *
+ * @author Marios
+ */
+class UserModel {
+
+    /**
+     * Retrieves all the Users from the database.
+     * 
+     * @return mixed false on failure; a {@link mysqli_result}, otherwise
+     */
+    public static function getUsers() {
+        $sqc = new SqlQueryCreator();
+        $sqc->selectTableColumns('user', array('id', 'username'), array('', ''))
+            ->from('user')
+            ->createQuery();
+        $dbc = new dbConnection();
+        
+        return $dbc->execute_query($sqc->getQuery());
+    }
+    
+}
+
+?>
