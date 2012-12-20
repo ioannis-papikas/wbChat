ed9a150024c07594de382cd27f00dcdf458a1e45
Marios Sotiropoulos <marios_sotir@hotmail.com>
2012-12-21 01:12:44 +0200

    Add constructor to sqlQuery.

--- system/core/database/sqlQuery.php
+++ system/core/database/sqlQuery.php
@@ -1,22 +1,27 @@
 <?php
+
 /*
  * Title : SQL Creator
  * Description : Creates SQL queries
  *
  */
 
-class sqlQuery
-{
-	private $query;
-	
-    public function set_query($query)
-	{
-     	$this->query = $query;
+class sqlQuery {
+
+    private $query;
+
+    public function sqlQuery($query) {
+        $this->query = $query;
     }
-	
-	public function get_query()
-	{
-		return $this->query;
-	}
+    
+    public function set_query($query) {
+        $this->query = $query;
+    }
+
+    public function get_query() {
+        return $this->query;
+    }
+
 }
+
 ?>
