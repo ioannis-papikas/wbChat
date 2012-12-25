63d7e1027591e432d0b4972eb8fadb6f276e5df7
mariossotir <mai1211@uom.gr>
2012-12-25 17:41:38 +0200

    add threadtypemodel.php
    Signed-off-by: mariossotir <mai1211@uom.gr>

---
+++ system/core/domain/threadtypemodel.php
@@ -0,0 +1,52 @@
+<?php
+
+// Imports
+importer::importCore("domain::model");
+
+/**
+ * 
+ *
+ * @author John
+ * @author Marios
+ */
+class ThreadTypeModel extends Model {
+    
+    /**
+     * 
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->table = 'threadtype';
+    }
+    
+    /**
+     * Returns the ThreadType with the specified description.
+     * 
+     * @param string $description
+     * @return array|null The row of the database of the ThreadType with the specified
+     * description or null if no such ThreadType exists.
+     * @throws InvalidArgumentException if the description is null
+     */
+    public function findByDescription($description) {
+        if ($description === null) {
+            throw new InvalidArgumentException(
+                    'Cannot search for ThreadType with empty description.');
+        }
+        
+        $sqc = new SqlQueryCreator();
+        $sqc->selectTableColumn($this->table, 'id')
+                ->from($this->table)
+                ->where('`description` = "' . $description . '"')
+                ->createQuery();
+
+        $threadType = null;
+        $resultSet = $this->getDbConnection()->execute_query(new sqlQuery($sqc->getQuery()));
+        if ($resultSet) {
+            $threadType = $resultSet->fetchAssoc();
+        }
+
+        return $threadType;
+    }
+}
+
+?>
